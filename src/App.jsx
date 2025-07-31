b90caf641941d9d30952a62f9f200ed941450499
Anoop Singh <anoopsinghssj3@gmail.com>
2025-07-31 20:36:02 +0530

    sf

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js'; // Optional for tooltips/popovers
+
+
+function App() {
+
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline bg-blue-500 text-white">
+        Hello world! tailwind
+      </h1>
+      <h1 className="text-3xl font-bold underline bg-dark text-white">
+        Hello world! bootstrap
+      </h1>
+
+      <h3 className="bg-secondary">Anoop</h3> 
+    </>
+  )
+}
+
+export default App
